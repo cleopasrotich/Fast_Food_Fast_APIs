e5465a7f2acbc73bb182dabfd43eadc41f9e13b0
cleopasrotich <cleopasrotich81@gmail.com>
2018-09-08 15:42:26 +0300

    fix: changed single quotes to double

--- app.py
+++ app.py
@@ -4,44 +4,44 @@ app = Flask(__name__)
 
 orders = [
     {
-        'id': 1,
-        'order_id': 'V2001',
-        'customer': 'John Wick',
-        'summary': '2 chips, 1/4 chicken',
-        'location': 'Juja',
-        'time': '1 hour ago',
+        "id": 1,
+        "order_id": "V2001",
+        "customer": "John Wick",
+        "summary": "2 chips, 1/4 chicken",
+        "location": "Juja",
+        "time": "1 hour ago"
     },
     {
-        'id': 2,
-        'order_id': 'V2511',
-        'customer': 'Beverly Mideva',
-        'summary': '1 Pizza Large, Latte',
-        'location': 'Juja',
-        'time': '2 hours ago',
+        "id": 2,
+        "order_id": "V2511",
+        "customer": "Beverly Mideva",
+        "summary": "1 Pizza Large, Latte",
+        "location": "Juja",
+        "time": "2 hours ago"
     },
     {
-        'id': 3,
-        'order_id': 'V4000',
-        'customer': 'Christene Mwangi',
-        'summary': 'Expresso',
-        'location': 'Westlands',
-        'time': '6 hours ago',
+        "id": 3,
+        "order_id": "V4000",
+        "customer": "Christene Mwangi",
+        "summary": "Expresso",
+        "location": "Westlands",
+        "time": "6 hours ago"
     },
     {
-        'id': 4,
-        'order_id': 'V2288',
-        'customer': 'Tony Stark',
-        'summary': '1 Pizza Large',
-        'location': 'Juja',
-        'time': '1 day ago',
+        "id": 4,
+        "order_id": "V2288",
+        "customer": "Tony Stark",
+        "summary": "1 Pizza Large",
+        "location": "Juja",
+        "time": "1 day ago"
     },
     {
-        'id': 5,
-        'order_id': 'V2191',
-        'customer': 'Cleopas Rotich',
-        'summary': 'French Fries',
-        'location': 'Juja',
-        'time': '6 days ago',
+        "id": 5,
+        "order_id": "V2191",
+        "customer": "Cleopas Rotich",
+        "summary": "French Fries",
+        "location": "Juja",
+        "time": "6 days ago"
     }
 ]
 
@@ -49,7 +49,7 @@ orders = [
 @app.route('/Fast_Food_Fast/api/v1/orders', methods=['GET'])
 def get_orders():
     return jsonify(
-        {'orders': orders}
+        {"orders": orders}
     )
 
 
